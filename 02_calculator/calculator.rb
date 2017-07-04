e92d4cfe3558a279d2b0cfb20abf565d7204324b
Randy <rpersun@hotmail.com>
2017-07-04 16:30:19 -0400

    Calculator passes all tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
-#write your code here
+def add(a,b)
+  a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+def multiply(a,b,c = 1,d = 1)
+  a *b * c * d
+end
+
+def power(a,b)
+  a ** b
+end
+
+def factorial(a)
+  ans = 1
+  if a == 0
+    ans = ans
+  else
+    while a > 0
+      ans = ans * a
+      a = a - 1
+    end
+  end
+  return ans
+end
+
