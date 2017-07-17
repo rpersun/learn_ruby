4a9f409c276ccb8eccef5b947005a3ead1e866ee
Randy <rpersun@hotmail.com>
2017-07-17 05:17:23 -0400

    Simon says complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
-#write your code here
+def echo(input)
+  input
+  
+end
+
+def shout(input)
+  input.upcase
+  
+end
+
+def repeat(a,num = 2)
+  ans = ''
+  while num > 0
+    ans = ans + a + ' '
+    num -= 1
+  end
+  ans.rstrip
+end
+
+def start_of_word(a,num = 1)
+  a.slice(0,num)
+end
+
+def first_word(a)
+  b = a.split
+  b[0]
+end
+
+def titleize(a)
+  words = a.split.map do |w|
+    if %w(the and over).include?(w)
+      w
+    else
+    w.capitalize 
+    end
+  end
+    words.first.capitalize!
+    words.join(' ')
+  
+  
+end
