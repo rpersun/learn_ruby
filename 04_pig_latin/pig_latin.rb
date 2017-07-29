4f7e724a6c8f67bfef70ff7bd0e650952f42ec50
Randy <rpersun@hotmail.com>
2017-07-29 14:41:03 -0400

    added pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,58 @@
-#write your code here
+def translate(sentence)
+  words = sentence.split(" ")
+  
+  fullans = ""
+  number_of_words = words.count
+  words.each do |word|  
+    letters = word.split(//)
+    #letters[0..1].join.upcase
+    
+    if %w(a e i o u).include? letters[0]
+      ans = letters.join + 'ay'
+      
+    else
+      case 
+        # looking for QU
+      when letters[0..1].join.upcase == 'QU'
+        first = letters[0..1].join
+        letters.slice!(0,2)
+        ans = letters.join + first + "ay"
+
+        # looking for squ
+      when letters[0..2].join.upcase == 'SQU'
+        first = letters[0..2].join
+        letters.slice!(0,3)
+        ans = letters.join + first + "ay"
+
+        #first three characters are consonants
+      when  letters[0] =~ /[^aeiou]/ && letters[1] =~ /[^aeiou]/ && letters[2] =~ /[^aeiou]/
+          first = letters[0..2].join
+        letters.slice!(0,3)
+        ans = letters.join + first + "ay"
+
+        #first two letters are consonants
+      when  letters[0] =~ /[^aeiou]/ && letters[1] =~ /[^aeiou]/
+          first = letters[0..1].join
+        letters.slice!(0,2)
+        ans = letters.join + first + "ay"
+
+        #first letter is a consonant
+      when  letters[0] =~ /[^aeiou]/ 
+          first = letters[0]
+        letters.slice!(0,1)
+        ans = letters.join + first + "ay"
+      end
+    end
+    if number_of_words > 1
+      fullans += ans + " "
+    else
+      fullans += ans 
+    end
+  end
+  fullans.rstrip
+end
+
+
+
+
+
