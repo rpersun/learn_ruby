d490b0cf7a4551c1e181a5fcac3abced56975cf1
Randy <rpersun@hotmail.com>
2017-08-06 15:59:39 -0400

    exercise book titles complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
 # write your code here
+  attr_accessor :title
+  # def initialize(title)
+  #   @title = title
+  # end
+
+
+  def title
+    
+    new_title = @title.split(' ')
+    new_title[0].capitalize!
+    new_title.each do |word|
+      word.downcase
+      if word == 'a' || word == 'and' || word == 'the' || 
+        word == 'of' || word == 'an' || word == 'or' || word == 'in'
+        word = word
+      else
+        word.capitalize!
+      end
+    end
+    new_title.join(" ")
+  end
 end
+
