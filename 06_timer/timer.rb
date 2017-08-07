0cde2f13e555a11c73e73ad4074c87a39735e90d
Randy <rpersun@hotmail.com>
2017-08-06 20:00:18 -0400

    finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padding num
+    if num < 10
+      return "0#{num}"
+    else
+      return "#{num}"
+    end
+  end
+
+  def time_string
+    
+    hours = (@seconds / 3600)
+    minutes = (@seconds / 60) % 60
+    seconds = (@seconds) % 60
+    return "#{padding(hours)}:#{padding(minutes)}:#{padding(seconds)}"
+    
+  end
 end
+
+
+
